048c8625d793ef32c305233d324555a0920c68c7
Kévin Begot <kevinbegot@MacBook-Pro-de-Kevin.local>
2023-11-27 16:04:01 +0100

    [ADD] exo1 and diagram builder

---
+++ exo1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace VehicleManagement
+{
+public class VehiculeElectrique
+  {
+      public string Model { get; set; }
+      public string Type { get; set; }
+      public string Year { get; set; }
+
+  }
+ 
+  public class VehiculeEssence 
+  {
+      public string Model { get; set; }
+      public string Type { get; set; }
+      public string Year { get; set; }
+  }
+ 
+  public interface IFabriqueVehicule
+  {
+      VehiculeElectrique CreateVehiculeElectrique();
+      VehiculeEssence CreateVehiculeEssence();
+  }
+ 
+ 
+  public class fabriqueVehicule : IFabriqueVehicule
+  {
+      public VehiculeElectrique CreateVehiculeElectrique()
+      {
+          return new VehiculeElectrique();
+      }
+
+        public VehiculeEssence CreateVehiculeEssence()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Catalogue
+    {
+        static void Main(string[] args)
+        {
+            IFabriqueVehicule factory = new fabriqueVehicule();
+
+            VehiculeElectrique vehiculeElectrique = factory.CreateVehiculeElectrique();
+            vehiculeElectrique.Model = "Model S";
+            vehiculeElectrique.Year = "2023";
+            vehiculeElectrique.Type = "Electrique";
+
+            VehiculeEssence vehiculeEssence = factory.CreateVehiculeEssence();
+            vehiculeEssence.Model = "F-150";
+            vehiculeEssence.Year = "2023";
+            vehiculeEssence.Type = "Essence";
+
+
+            Console.WriteLine("Véhicule électrique :");
+            Console.WriteLine($"Model: {vehiculeElectrique.Model}, Year: {vehiculeElectrique.Year}");
+
+            Console.WriteLine("\nVéhicule essence :");
+            Console.WriteLine($"Model: {vehiculeEssence.Model}, Year: {vehiculeEssence.Year}");
+         
+
+            Console.ReadKey();
+        }
+    }
+}
