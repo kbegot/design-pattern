5e0239b53f537ab42991967ad9f68e7d6bebde52
Kévin Begot <kevinbegot@MacBook-Pro-de-Kevin.local>
2023-11-29 10:13:14 +0100

    [ADD] init library

--- biblio/src/app.module.ts
+++ biblio/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LibraryModule } from './library/library.module';
 
 @Module({
-  imports: [],
+  imports: [LibraryModule],
   controllers: [AppController],
   providers: [AppService],
 })
