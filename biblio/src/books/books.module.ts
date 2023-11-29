429576ace7cd96e5174b849ba475c39f3171bd20
Kévin Begot <kevinbegot@MacBook-Pro-de-Kevin.local>
2023-11-29 14:28:43 +0100

    [ADD] book categories ans author on db

--- biblio/src/books/books.module.ts
+++ biblio/src/books/books.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BooksController } from './books.controller';
 import { BooksService } from './books.service';
+import { BookSchema } from './book.model';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Book', schema: BookSchema }])],
   controllers: [BooksController],
   providers: [BooksService],
 })
--- biblio/src/categories/categories.module.ts
+++ biblio/src/categories/categories.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { CategorySchema } from './categorie.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Category', schema: CategorySchema }]),
+  ],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
